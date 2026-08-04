e92ea40995c215411a135fdda40e6abbbc4db5fb
Sakthivel Balaji Perumal <sakthi.perumal1@gmail.com>
2026-08-04 08:39:44 +0100

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        int bit;
+        uint32_t y{};
+        for(int i = 0; i < 32; i++) {
+            bit = n & 1;
+            y<<=1;
+            y = y | bit;
+            n >>=1;
+        }
+        return y;
+    }
+};
